0e85dae88708834af09617ad07bcd814b9d48bb1
jakecleary <shout@jakecleary.net>
2015-05-05 14:43:27 +0100

    :computer: Init commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?hh //strict
+
+class Welcome
+{
+    public string $hello = 'Welcome';
+
+    final public function __construct()
+    {
+        echo $this->hello;
+    }
+}
+
+new Hello;
---
+++ puphpet/readme.md
@@ -0,0 +1,3 @@
+# hack-mvc
+
+Just me having a play around with hack-lang, and maybe building a basic MVC framework.
